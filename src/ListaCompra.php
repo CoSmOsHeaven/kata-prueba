7f691ddcf14985db64f4e28fe92276181f3ae68b
Ruben Zubasti <rubenix2.0@gmail.com>
2025-04-01 13:12:28 +0200

    [refactor] - Mantengo el estandar de llaves en la linea siguiente del if o funcion

--- src/ListaCompra.php
+++ src/ListaCompra.php
@@ -11,33 +11,43 @@ class ListaCompra
         $this->listaCompraProductos = [];
         $this->listaCompraCantidades = [];
     }
-    public function execute($command) : string{
+    public function execute($command) : string
+    {
         $splittedCommand = explode(" ", $command);
         $product = $splittedCommand[1];
         $result = "";
-        if(count($splittedCommand) == 2){
-            if(!in_array($product, $this->listaCompraProductos)){
+        if(count($splittedCommand) == 2)
+        {
+            if(!in_array($product, $this->listaCompraProductos))
+            {
                 $this->listaCompraProductos[] = $product;
                 $this->listaCompraCantidades[] = 1;
-            }else{
+            }
+            else
+            {
                 $index = array_search($product, $this->listaCompraProductos);
                 $this->listaCompraCantidades[$index] += 1;
             }
-            for($i = 0; $i < count($this->listaCompraProductos); $i++){
+            for($i = 0; $i < count($this->listaCompraProductos); $i++)
+            {
                 $result .= $this->listaCompraProductos[$i] . " x" . $this->listaCompraCantidades[$i] . ", ";
             }
 
             return rtrim($result, ", ");
         }
         $quantity = (int)$splittedCommand[2];
-        if(!in_array($product, $this->listaCompraProductos)){
+        if(!in_array($product, $this->listaCompraProductos))
+        {
             $this->listaCompraProductos[] = $product;
             $this->listaCompraCantidades[] = $quantity;
-        }else{
+        }
+        else
+        {
             $index = array_search($product, $this->listaCompraProductos);
             $this->listaCompraCantidades[$index] += $quantity;
         }
-        for($i = 0; $i < count($this->listaCompraProductos); $i++){
+        for($i = 0; $i < count($this->listaCompraProductos); $i++)
+        {
             $result .= $this->listaCompraProductos[$i] . " x" . $this->listaCompraCantidades[$i] . ", ";
         }
 
--- tests/ListaCompraTest.php
+++ tests/ListaCompraTest.php
@@ -16,7 +16,8 @@ class ListaCompraTest extends TestCase
     /**
      * @test
      */
-    public function givenStringAñadirPanReturnsStringWithPan() : void{
+    public function givenStringAñadirPanReturnsStringWithPan() : void
+    {
         $result = $this->listaCompra->execute("añadir pan");
 
         $this->assertEquals("pan x1", $result);
@@ -25,7 +26,8 @@ class ListaCompraTest extends TestCase
     /**
      * @test
      */
-    public function givenStringAñadirPan2ReturnsStringWithPanx2() : void{
+    public function givenStringAñadirPan2ReturnsStringWithPanx2() : void
+    {
         $result = $this->listaCompra->execute("añadir pan 2");
 
         $this->assertEquals("pan x2", $result);
@@ -34,7 +36,8 @@ class ListaCompraTest extends TestCase
     /**
      * @test
      */
-    public function givenStringAñadirPan2WithExistingPanx1ReturnsStringWithPanx3(): void{
+    public function givenStringAñadirPan2WithExistingPanx1ReturnsStringWithPanx3(): void
+    {
         $result = $this->listaCompra->execute("añadir pan 1");
         $result = $this->listaCompra->execute("añadir pan 2");
 
@@ -44,7 +47,8 @@ class ListaCompraTest extends TestCase
     /**
      * @test
      */
-    public function givenStringAñadirPan2WithExistingLechex1ReturnsStringWithLechex1Panx2(): void{
+    public function givenStringAñadirPan2WithExistingLechex1ReturnsStringWithLechex1Panx2(): void
+    {
         $result = $this->listaCompra->execute("añadir leche 1");
         $result = $this->listaCompra->execute("añadir pan 2");
 
